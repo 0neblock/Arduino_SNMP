5dd6a9927d83e743784ec6cce136cdf9f6b5de67
Aidan Cyr <cyraidan@gmail.com>
2017-07-11 17:08:42 +1000

    Updated example to include an snmpset

--- examples/ESP_SNMP/ESP_SNMP.ino
+++ examples/ESP_SNMP/ESP_SNMP.ino
@@ -6,9 +6,10 @@ const char* ssid = "SSID";
 const char* password = "PASSWORD";
 
 WiFiUDP udp;
-SNMPAgent snmp = SNMPAgent("public");
+SNMPAgent snmp = SNMPAgent("public");  // Starts an SMMPAgent instance with the community string 'public'
 
 int changingNumber = 1;
+int settableNumber = 0;
 
 void setup(){
     Serial.begin(115200);
@@ -35,9 +36,19 @@ void setup(){
     
     // Using your favourite snmp tool:
     // snmpget -v 1 -c public <IP> 1.3.6.1.4.1.5.0
+    
+    // you can accept SET commands with a pointer to an integer (or string)
+    snmp.addIntegerHandler(".1.3.6.1.4.1.5.1", &settableNumber, true);
+    
+    // snmpset -v 1 -c public <IP> 1.3.6.1.4.1.5.0 i 99
+    
 }
 
 void loop(){
     snmp.loop(); // must be called as often as possible
+    if(snmp.setOccurred){
+        Serial.printf("Number has been set to value: %i", settableNumber);
+        snmp.resetSetOccurred();
+    }
     changingNumber++;
 }
